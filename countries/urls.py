35e9295429cdb1503488bea14e55a614e7c4ded6
michaelborhlar <michealadefehinti09@gmail.com>
2026-04-16 17:38:31 +0100

    Update urls.py

--- countries/urls.py
+++ countries/urls.py
@@ -1,16 +1,6 @@
 from django.urls import path
-from .views import (
-    RefreshCountriesView,
-    CountryListView,
-    CountryDetailView,
-    StatusView,
-    SummaryImageView
-)
+from .views import ClassifyView
 
 urlpatterns = [
-    path("countries/refresh", RefreshCountriesView.as_view(), name="refresh_countries"),
-    path("countries/image", SummaryImageView.as_view(), name="country_image"),
-    path("countries/<str:name>", CountryDetailView.as_view(), name="country_detail"),
-    path("countries", CountryListView.as_view(), name="list_countries"),
-    path("status", StatusView.as_view(), name="status"),
+    path("classify", ClassifyView.as_view(), name="classify"),
 ]
